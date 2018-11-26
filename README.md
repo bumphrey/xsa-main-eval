a8548cbb887d93510bf8b57979b9b8a273226d7a
Peter Kain <peter.kain@systemagmbh.de>
2018-11-26 15:07:34 +0100

    Test deploy, run, undeploy of simple java main class

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # xsa-main-eval
+------------------
+steps to test:
+
+mvn clean package
+xs push xsa-main-eval
+xs logs xsa-main-eval --recent
+xs env xsa-main-eval
+xs delete xsa-main-eval
--- src/main/java/com/systemagmbh/xsa/eval/Main.java
+++ src/main/java/com/systemagmbh/xsa/eval/Main.java
@@ -2,9 +2,16 @@ package com.systemagmbh.xsa.eval;
 
 public class Main {
 	
-	public void main(String args[]){
-		System.out.println("Started");
+	public static void main(String args[]){
+		System.out.println("xsa-main-eval Started");
 		
+		System.out.println("xsa-main-eval number of command line arguments : " + args.length);
+		String arg = null;
+		for(int i=0; i < args.length; i++) {
+			System.out.println("arg["+ i + "] : "+ args[i]);
+		}
+		
+		System.out.println("xsa-main-eval Ending");
 	}
 	
 }
